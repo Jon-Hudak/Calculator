ae553373d1ad1187629d5ce2fe5f406c2d654ef8
Jon-Hudak <shigshagor@gmail.com>
2022-06-16 00:59:43 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import NumPad from "./NumPad";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="calculator">
+        <NumPad />
+      </div>
+    </div>
+  );
+}
+
+export default App;
