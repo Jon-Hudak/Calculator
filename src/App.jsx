e06effc472a4b44b9ae7c2f56a895e3ba35d5be1
Jon-Hudak <shigshagor@gmail.com>
2022-06-26 21:21:03 -0400

    Added the rest of the numpad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Button from "./Button";
 import NumPad from "./NumPad";
-
+import { Container, Row } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
       <div id="calculator">
-        <NumPad />
+        <Container id="btnCont" className="px-0">
+          <Row>
+            <NumPad />
+          </Row>
+        </Container>
       </div>
     </div>
   );
--- src/NumPad.jsx
+++ src/NumPad.jsx
@@ -1,16 +1,138 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Button from "./Button";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
 export default function NumPad() {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col xs={6}><Button id="AC" text="AC"/></Col>
-          <Col xs={3}><Button id="divide" text="/"/> </Col>
-          <Col xs={3}><Button id="multiply" text="X"/></Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Col className="px-0" xs={6}>
+        <Button className="btn btn-danger btn-block rounded-0" id="AC">
+          {" "}
+          AC
+        </Button>
+      </Col>
+      
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block btn-secondary rounded-0" id="divide">
+          /
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block btn-secondary rounded-0" id="multiply">
+          X
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="seven">
+          7
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="eight">
+          8
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="nine">
+          9
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block btn-secondary rounded-0 " id="minus">
+          -
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="four">
+          4
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="five">
+          5
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="six">
+          6
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block btn-secondary rounded-0" id="plus">
+          +
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="seven">
+          7
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="eight">
+          8
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="nine">
+          9
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button
+          className="btn btn-block btn-secondary rounded-0 oper"
+          id="minus"
+        >
+          -
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="one">
+          1
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="two">
+          2
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="three">
+          3
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block btn-primary rounded-0" id="equal">
+          =
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={6}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="zero">
+          0
+        </Button>
+      </Col>
+
+      <Col className="px-0" xs={3}>
+        <Button className="btn btn-block rounded-0 btn-dark" id="dec">
+          .
+        </Button>
+      </Col>
+    </>
   );
 }
