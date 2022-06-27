9478aaba966a4d3d50804166cf873fd1e9c610f3
Jon-Hudak <shigshagor@gmail.com>
2022-06-26 23:28:08 -0400

    feat: Added state and hooks for display

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import NumPad from "./NumPad";
 import { Container, Row } from "react-bootstrap";
 import Display from "./Display";
+import { useState } from "react";
 function App() {
+
+  const [display, setDisplay]=useState('0');
+  const [answer, setAnswer]=useState('0');
+
   return (
     <div className="App">
       
         <Container id="calculator" className="">
 
           <Row>
-            <Display/>
+            <Display display={display} answer={answer}/>
           </Row>
           <Row>
-            <NumPad />
+            <NumPad display={display} setDisplay={setDisplay} setAnswer={setAnswer}/>
           </Row>
         </Container>
       
--- src/Display.jsx
+++ src/Display.jsx
@@ -3,11 +3,11 @@ import { Col } from 'react-bootstrap'
 
 
 
-export default function Display() {
+export default function Display({display, answer}) {
   return (
     <>
-     <Col xs={12}id="display" className=" text-warning py-0 my-0">123</Col>
-     <Col id="answer" className=" text-light py-0 my-0">432</Col>
+     <Col xs={12}id="display" className=" text-warning py-0 my-0">{display}</Col>
+     <Col id="answer" className=" text-light py-0 my-0">{answer} </Col>
     </>
   )
 }
