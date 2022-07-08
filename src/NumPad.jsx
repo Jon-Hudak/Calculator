93fb5512c20ed34093f094511dca49a1490d6870
Jon-Hudak <shigshagor@gmail.com>
2022-07-08 02:16:14 -0400

    fix: fixed broken multiplication/division

--- src/NumPad.jsx
+++ src/NumPad.jsx
@@ -11,9 +11,9 @@ export default function NumPad({ display, answer, setDisplay, setAnswer }) {
 
   const equals = (input) => {
     let newAns = 0;
-    let newDisp=input;
-    let breakout=false;
-    while (/[x/]/.test(input) && breakout==false) {
+    let newDisp = input;
+    let breakout = false;
+    while (/[x/]/.test(input) && breakout == false) {
       //checks if multiplication or division first
       // const match = input.match(/(-?\d(?:.\d+)?)+([/x])([0-9](?:.\d+)?)/);
       let match = input.match(/([0-9]+)([x/])([0-9]+)/);
@@ -26,81 +26,77 @@ export default function NumPad({ display, answer, setDisplay, setAnswer }) {
           newAns = parseInt(num1) * parseInt(num2);
           input = input.replace(exp, newAns.toString());
           console.log(input);
-          
+
           break;
         case "/":
           if (num2 == 0) {
-            console.log('div zero')
-            newAns="Negative Infinity";
-            breakout=true;
+            console.log("div zero");
+            newAns = "Negative Infinity";
+            breakout = true;
             break;
           }
           console.log("div");
           newAns = parseInt(num1) / parseInt(num2);
           input = input.replace(exp, newAns.toString());
-          console.log(input);          
+          console.log(input);
           break;
 
         default:
           throw new Error();
       }
     }
+    console.log(newAns);
+    breakout = false;
+    while (/[+-]/.test(input) && breakout == false) {
+      //checks if multiplication or division first
+      // const match = input.match(/(-?\d(?:.\d+)?)+([/x])([0-9](?:.\d+)?)/);
+      let match = input.match(/([0-9]+)([+-])([0-9]+)/);
+      console.log(match);
+      let [exp, num1, operator, num2] = match;
 
+      switch (operator) {
+        case "+":
+          console.log("no");
+          newAns = parseInt(num1) + parseInt(num2);
+          input = input.replace(exp, newAns.toString());
+          console.log(input);
 
-      while (/[+-]/.test(input) && breakout==false) {
-        //checks if multiplication or division first
-        // const match = input.match(/(-?\d(?:.\d+)?)+([/x])([0-9](?:.\d+)?)/);
-        let match = input.match(/([0-9]+)([+-])([0-9]+)/);
-        console.log(match);
-        let [exp, num1, operator, num2] = match;
-  
-        switch (operator) {
-          case "+":
-            console.log("no");
-            newAns = parseInt(num1) + parseInt(num2);
-            input = input.replace(exp, newAns.toString());
-            console.log(input);
-            
-            break;
-          case "-":
-            console.log("div");
-            newAns = parseInt(num1) - parseInt(num2);
-            input = input.replace(exp, newAns.toString());
-            console.log(input);          
-            break;
-  
-          default:
-            throw new Error();
-        }
+          break;
+        case "-":
+          console.log("div");
+          newAns = parseInt(num1) - parseInt(num2);
+          input = input.replace(exp, newAns.toString());
+          console.log(input);
+          break;
 
-
-      return newAns;
+        default:
+          throw new Error();
+      }
     }
-    //  if (true)){
+    return newAns;
 
-    //  }
   };
 
   const clickHandler = (e) => {
     let btnPressed = e.target.innerText;
-    let newDisp=display;
-    let newAns=answer;
-    let equalsLP=equalsLastPressed;
+    let newDisp = display;
+    let newAns = answer;
+    let equalsLP = equalsLastPressed;
 
     switch (btnPressed) {
       case "AC":
-        newDisp="0";
-        newAns="0";
-        equalsLP=false;
+        newDisp = "0";
+        newAns = "0";
+        equalsLP = false;
         break;
 
       case "=":
-        if (equalsLastPressed == false) {
+        if (equalsLastPressed === false) {
           console.log("TODO Equals");
-          newAns=equals(display);
-          newDisp=`${display}=${newAns}`
+          newAns = equals(display);
+          newDisp = `${display}=${newAns}`;
         }
-        equalsLP=true;
+        equalsLP = true;
         break;
 
       case ".":
@@ -115,39 +111,36 @@ export default function NumPad({ display, answer, setDisplay, setAnswer }) {
       case "-":
       case "+":
         if (equalsLastPressed === true) {
-          newDisp='';
+          newDisp = "";
         }
         if (display !== "0" && display !== "-") {
           if (!display.match(/.+[/+\-x]$/)) {
-            newDisp=display + btnPressed;
+            newDisp = display + btnPressed;
           }
           if (display.match(/.*[/+\-x]$/)) {
-            newDisp=display.replaceAll(/[/+-x]$/g, btnPressed);
+            newDisp = display.replaceAll(/[/+-x]$/g, btnPressed);
           }
         } else {
-          newDisp="-";
+          newDisp = "-";
         }
-        equalsLP=false;
+        equalsLP = false;
         break;
 
       default:
         //assuming all other cases are accounted for, must be a number
 
         if (equalsLastPressed) {
-         newDisp="";
+          newDisp = "";
         }
 
-        newDisp=checkLeadingZero(display + btnPressed);
+        newDisp = checkLeadingZero(display + btnPressed);
 
-        equalsLP=false;
+        equalsLP = false;
         break;
     }
     setDisplay(newDisp);
     setAnswer(newAns);
     setEqualsLastPressed(equalsLP);
-
-
-
   };
 
   return (
